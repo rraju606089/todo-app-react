75b57d71ec00bb746712f0a347e3278511bd8c44
rraju606089 <Jnanaraj DH>
2026-06-30 12:21:04 +0530

    Deploy projects

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/https://github.com/rraju606089/todo-app-react.git/",
   plugins: [react()],
+  base:'/http://localhost:5173//'
 })
