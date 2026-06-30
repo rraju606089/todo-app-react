2f348b3bcaa8fe130ec983054d83426ea7cd4975
rraju606089 <Jnanaraj DH>
2026-06-30 12:39:07 +0530

    fix the githupage

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/https://github.com/rraju606089/todo-app-react.git/'
+  base:'/todo-app-react/',
 })
